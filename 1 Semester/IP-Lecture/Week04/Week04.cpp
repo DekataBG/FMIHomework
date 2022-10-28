7677a71cf5e8865be6831af8eb8949220bcfdd51
DekataBG <dekatawee@abv.bg>
2022-10-28 20:26:22 +0300

    Week04 Lecture Skeleton

---
+++ 1 Semester/IP-Lecture/Week04/Week04.cpp
@@ -0,0 +1,61 @@
+// Problems from section 3.2 from https://github.com/stranxter/lecture-notes/blob/master/homeworks/Problems%20in%20Programming.pdf
+
+#include <iostream>
+
+void problem3_8()
+{
+    char symbols[255];
+}
+
+void problem3_9()
+{
+    char symbols[255];
+
+    std::cin.getline(symbols, 255);
+    int length = sizeof(symbols);
+
+    std::cout << length;
+}
+
+void problem3_10()
+{
+}
+
+void problem3_11()
+{
+}
+
+void problem3_12()
+{
+}
+
+void problem3_13()
+{
+}
+
+void problem3_14()
+{
+}
+
+void problem3_15()
+{
+}
+
+void problem3_16()
+{
+}
+
+int main()
+{
+    // problem3_8();
+    problem3_9();
+    // problem3_10();
+    // problem3_11();
+    // problem3_12();
+    // problem3_13();
+    // problem3_14();
+    // problem3_15();
+    // problem3_16();
+
+    return 0;
+}
