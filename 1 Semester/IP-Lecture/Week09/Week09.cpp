b3720d3e71dedb59edc2d4092bddf15d7322943b
DekataBG <dekatawee@abv.bg>
2022-12-10 11:36:35 +0200

    Week09 Lecture Sections 4.1 and 4.2

---
+++ 1 Semester/IP-Lecture/Week09/Week09.cpp
@@ -0,0 +1,289 @@
+// Problems from section 4 from https://github.com/stranxter/lecture-notes/blob/master/homeworks/Problems%20in%20Programming.pdf
+
+#include <iostream>
+
+void swapArrayElements(int array1[], int array2[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        int temp = array1[i];
+        array1[i] = array2[i];
+        array2[i] = temp;
+    }
+}
+
+void problem4_1()
+{
+    int size;
+    int array1[100], array2[100];
+
+    std::cout << "Enter size: ";
+    std::cin >> size;
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "Enter element for array1: ";
+        std::cin >> array1[i];
+
+        std::cout << "Enter element for array2: ";
+        std::cin >> array2[i];
+    }
+
+    std::cout << "Array 1 before swap: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << array1[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Array 2 before swap: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << array2[i] << " ";
+    }
+    std::cout << std::endl;
+
+    swapArrayElements(array1, array2, size);
+
+    std::cout << "Array 1 after swap: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << array1[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Array 2 after swap: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << array2[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void problem4_2()
+{
+    int a, b;
+
+    std::cout << "Enter a: ";
+    std::cin >> a;
+
+    std::cout << "Enter b: ";
+    std::cin >> b;
+
+    swap(a, b);
+
+    std::cout << "After swap: \na: " << a << "\nb: " << b << std::endl;
+}
+
+bool duplicates(long *pointers[])
+{
+    int ctr = 0;
+
+    while (pointers[ctr])
+    {
+        int ctr2 = ctr + 1;
+        while (pointers[ctr2])
+        {
+            if (*pointers[ctr] == *pointers[ctr2])
+            {
+                return true;
+            }
+
+            ctr2++;
+        }
+
+        ctr++;
+    }
+
+    return false;
+}
+
+void problem4_3()
+{
+    int n;
+    long values[50];
+    long *pointers[50];
+
+    std::cout << "Enter n: ";
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "Enter element: ";
+        std::cin >> values[i];
+        pointers[i] = &values[i];
+    }
+    pointers[n] = nullptr;
+
+    std::cout << "Equal elements: " << std::boolalpha << duplicates(pointers) << std::endl;
+}
+
+bool containsElement(int array[], int size, int element)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] == element)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool arraysContainElement(int *arrays[], int npointers, int arrlenghts[], int element)
+{
+    for (int i = 0; i < npointers; i++)
+    {
+        if (!containsElement(arrays[i], arrlenghts[i], element))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool commonel(int *arrays[], int npointers, int arrlenghts[])
+{
+    for (int i = 0; i < arrlenghts[0]; i++)
+    {
+        if (arraysContainElement(arrays, npointers, arrlenghts, arrays[0][i]))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void problem4_4()
+{
+    int matrix[50][50];
+    int npointers;
+    int arrlenghts[50];
+    int *arrays[50];
+
+    std::cout << "Enter n: ";
+    std::cin >> npointers;
+
+    for (int i = 0; i < npointers; i++)
+    {
+        int size;
+
+        std::cout << "Enter size for array " << i + 1 << ": ";
+        std::cin >> arrlenghts[i];
+
+        for (int j = 0; j < arrlenghts[i]; j++)
+        {
+            std::cout << "Enter element for array " << i + 1 << ": ";
+            std::cin >> matrix[i][j];
+        }
+
+        arrays[i] = matrix[i];
+    }
+
+    std::cout << "There are common elements: " << std::boolalpha
+              << commonel(arrays, npointers, arrlenghts) << std::endl;
+}
+
+bool isSubArrayOfArray(int arr1[], int size1, int arr2[], int size2)
+{
+    if (size2 > size1)
+    {
+        return false;
+    }
+
+    for (int i = 0; i < size2; i++)
+    {
+        if (!containsElement(arr1, size1, arr2[i]))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool isSubArrayOfAllArrays(int *arrays[], int npointers, int arrlenghts[], int arr[], int size)
+{
+    for (int i = 0; i < npointers; i++)
+    {
+        if (!isSubArrayOfArray(arrays[i], arrlenghts[i], arr, size))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool subarrays(int *arrays[], int npointers, int arrlenghts[])
+{
+    for (int i = 0; i < npointers; i++)
+    {
+        if (isSubArrayOfAllArrays(arrays, npointers, arrlenghts, arrays[i], arrlenghts[i]))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void problem4_5()
+{
+    int matrix[50][50];
+    int npointers;
+    int arrlenghts[50];
+    int *arrays[50];
+
+    std::cout << "Enter n: ";
+    std::cin >> npointers;
+
+    for (int i = 0; i < npointers; i++)
+    {
+        int size;
+
+        std::cout << "Enter size for array " << i + 1 << ": ";
+        std::cin >> arrlenghts[i];
+
+        for (int j = 0; j < arrlenghts[i]; j++)
+        {
+            std::cout << "Enter element for array " << i + 1 << ": ";
+            std::cin >> matrix[i][j];
+        }
+
+        arrays[i] = matrix[i];
+    }
+
+    for (int i = 0; i < npointers; i++)
+    {
+        for (int j = 0; j < arrlenghts[i]; j++)
+        {
+            std::cout << arrays[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "There is a common subarray: " << std::boolalpha
+              << subarrays(arrays, npointers, arrlenghts) << std::endl;
+}
+
+int main()
+{
+    // problem4_1();
+    // problem4_2();
+    // problem4_3();
+    // problem4_4();
+    // problem4_5();
+
+    return 0;
+}
