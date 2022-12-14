5db6f0a362b2b5a3db4ba045081278b16e920cd7
DekataBG <dekatawee@abv.bg>
2022-12-14 20:55:34 +0200

    Week 11 Lecture Section 5.1

--- 1 Semester/IP-Lecture/Week11/Week11.cpp
+++ 1 Semester/IP-Lecture/Week11/Week11.cpp
@@ -98,6 +98,194 @@ void problem5_7()
     std::cout << "The product is " << product(n, k, 1) << std::endl;
 }
 
+int sumOfDigits(int n, int k, int sum)
+{
+    if(n == 0)
+    {
+        return sum;
+    }
+    
+    return sumOfDigits(n / k, k, sum + n % k);
+}
+
+void problem5_10()
+{
+    int n, k;
+
+    std::cout << "Enter number: ";
+    std::cin >> n;
+
+    std::cout << "Enter base: ";
+    std::cin >> k;
+
+    std::cout << "The sum of the digits of " << n << " in base " 
+        << k << " is " << sumOfDigits(n, k, 0) << std::endl;
+}
+
+bool contains(int n, int k, int digits)
+{
+    if(n == 0)
+    {
+        return digits == 1;
+    }
+
+    if(n % 10 == k)
+    {
+        return true;
+    } 
+
+
+    return contains(n / 10, k, digits + 1);
+}
+
+void problem5_11()
+{
+    int n, k;
+
+    std::cout << "Enter n: ";
+    std::cin >> n;
+
+    std::cout << "Enter k: ";
+    std::cin >> k;
+
+    bool containsK = contains(n, k, 1);
+
+    std::cout << n << (containsK ? " contains " : " doesnt contain ") << k <<  std::endl;
+}
+
+bool isFibonacci(int n, int previous, int current)
+{
+    if(n <= previous)
+    {
+        return n == previous;
+    }
+
+    return isFibonacci(n, current, previous + current);
+}
+
+void problem5_19()
+{
+    int n;
+
+    std::cout << "Enter n: ";
+    std::cin >> n;
+
+    std::cout << n << (isFibonacci(n, 0, 1) ? " is " : " isnt ") 
+        << "Fibonacci digit" <<  std::endl;
+}
+
+int findBiggestElement(int array[], int n)
+{
+    if(n == 1)
+    {
+        return array[n - 1];
+    }
+
+    int biggestElement = findBiggestElement(array, n - 1);
+
+    return array[n - 1] >= biggestElement ? array[n - 1] : biggestElement;
+}
+
+void problem5_28()
+{
+    int n;
+    int array[50];
+
+    std::cout << "Enter n: ";
+    std::cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        std::cout << "Enter element: ";
+        std::cin >> array[i];
+    }
+
+    std::cout << "The biggest element is " << findBiggestElement(array, n) << std::endl;
+}
+
+void insertSorted(int x, int array[], int n)
+{
+    array[n] = array[n - 1];
+
+    if(x >= array[n - 1])
+    {
+        array[n] = x;
+        return;
+    }
+
+    insertSorted(x, array, n - 1);
+}
+
+void problem5_31()
+{
+    int n, element;
+    int array[50];
+
+    std::cout << "Enter n: ";
+    std::cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        std::cout << "Enter element: ";
+        std::cin >> array[i];
+    }
+
+    std::cout << "Enter element to insert: ";
+    std::cin >> element;
+
+    insertSorted(element, array, n);
+
+    for(int i = 0; i < n + 1; i++)
+    {
+        std::cout << array[i] << " ";
+    }
+}
+
+int compare(char string1[], char string2[])
+{
+    if (*string1 < *string2)
+    {
+        return -1;
+    }
+    else if (*string1 > *string2)
+    {
+        return 1;
+    }
+    else if (*string1 == *string2 && *string1 == '\0')
+    {
+        return 0;
+    }
+
+    return compare(string1 + 1, string2 + 1);
+}
+
+void problem5_34()
+{
+    char string1[50];
+    char string2[50];
+
+    std::cout << "Enter string 1: ";
+    std::cin.getline(string1, 50);
+
+    std::cout << "Enter string 2: ";
+    std::cin.getline(string2, 50);
+
+    int n = compare(string1, string2);
+
+    if (n < 0)
+    {
+        std::cout << "string 1 is before string 2" << std::endl;
+    }
+    else if (n == 0)
+    {
+        std::cout << "string 1 is equal to string 2" << std::endl;
+    }
+    else
+    {
+        std::cout << "string 1 is after string 2" << std::endl;
+    }
+}
+
 
 int main()
 {
@@ -105,6 +293,12 @@ int main()
     // problem5_3();
     // problem5_7();
     // problem5_7();
+    // problem5_10();
+    // problem5_11();
+    // problem5_19();
+    // problem5_28();
+    // problem5_31();
+    // problem5_34();
 
 
     return 0;
